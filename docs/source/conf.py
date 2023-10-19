bbbdb8b56c597ad4acbf17e22b500de7918c68a6
Jaemin Shin <72111485+mr-jaemin@users.noreply.github.com>
2023-10-19 16:23:08 -0400

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'mr-jaemin'
+copyright = '2023, Jaemin Shin'
+author = 'Jaemin Shin'
 
 release = '0.1'
 version = '0.1.0'
